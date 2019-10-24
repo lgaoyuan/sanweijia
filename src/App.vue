14c78cf37668da25dfe2975d4e487b0750ac99a9
lgaoyuan <lgaoyuan@csu.edu.cn>
2019-10-24 20:49:00 +0800

    npminstall

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-<!--    <div id="app">-->
-        <Main></Main>
-        <!--      <router-link to="/">Home21</router-link> |-->
-        <!--    <router-view/>-->
-<!--    </div>-->
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
-<style>
-    body {
-        margin: 0;
-            min-width: 1368px;
-            /*overflow: hidden;*/
-    }
-
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-</style>
 <script>
-    import Main from "@/views/Main";
-
-    export default {
-        components: {Main}
-    }
+export default {
+  name: 'App'
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
-import router from './config/router'
-import store from './config/store'
-import iview from 'iview'
-import 'iview/dist/styles/iview.css';
-import VueRouter from "vue-router";
-import less from 'less'
-import echarts from 'echarts'
-import axios from 'axios'
+import App from './App'
+import router from './router'
 
-Vue.use(axios);
-Vue.config.productionTip = false;
-Vue.use(iview);
-Vue.use(VueRouter);
-Vue.use(less);
-Vue.use(echarts);
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app');
+  components: { App },
+  template: '<App/>'
+})
